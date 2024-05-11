0dfc634c4b05ed9d6f5a7dd489fd790c67360abc
bri111 <brianall@hawaii.edu>
2024-05-10 22:07:13 -1000

    Update NavBar.jsx

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
-import { Meteor } from 'meteor/meteor';
-import { useTracker } from 'meteor/react-meteor-data';
+// import { Meteor } from 'meteor/meteor';
+// import { useTracker } from 'meteor/react-meteor-data';
 import { NavLink } from 'react-router-dom';
 // import { Roles } from 'meteor/alanning:roles';
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Container, Navbar } from 'react-bootstrap';
 // import { BoxArrowRight, PersonFill, PersonPlusFill } from 'react-bootstrap-icons';
 
-const NavBar = () => {
-
-  return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand as={NavLink} to="/">
-          <h2>Code Quest</h2>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      </Container>
-    </Navbar>
-  );
-};
+const NavBar = () => (
+  <Navbar bg="light" expand="lg">
+    <Container>
+      <Navbar.Brand as={NavLink} to="/">
+        <h2>Code Quest</h2>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    </Container>
+  </Navbar>
+);
 
 export default NavBar;
